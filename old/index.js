21af4c08a59eef43651801ea79fef241b11a6d74
Michael <43703002+michaelhoner@users.noreply.github.com>
2019-10-20 11:15:20 +0200

    minor fix

--- old/index.js
+++ old/index.js
@@ -21,6 +21,24 @@ window.addEventListener('beforeinstallprompt', (e) => {
   showInstallPromotion();
 });
 
+// Show prompt install
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+
 // Apple code
 // Detects if device is on iOS 
 const isIos = () => {
